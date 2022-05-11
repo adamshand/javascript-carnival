2fffaa94c0f69d4b598cd30fe1574bcb74f8f588
Adam Shand <adam@shand.net>
2022-05-11 15:30:47 +1200

    working with pointer, needs tidy still

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,8 +1,9 @@
 console.log('Dress The Clown!')
 
-let head = document.getElementById('head')
-dressing = ['head', 'body', 'shoes']
-let state = { focus: 1, head: 2, body: 3, shoes: 4 }
+const bodyAreas = ['head', 'body', 'shoes']
+let state = { focus: 1, head: 0, body: 0, shoes: 0 }
+
+printState()
 
 document.onkeydown = checkKey
 function checkKey(e) {
@@ -18,39 +19,72 @@ function checkKey(e) {
   }
 }
 
+function dressClown() {
+  let area = getBodyArea()
+  let item = getAreaItem(area)
+  let file = `images/${area}${item}.png`
+  let clothing = document.getElementById(area)
+  clothing.src = file
+}
+
+function moveArrow() {
+  for (let area of bodyAreas) {
+    let focus = document.getElementById(area + 'Focus')
+    focus.src = ''
+  }
+  let area = getBodyArea()
+  let focus = document.getElementById(area + 'Focus')
+  focus.src = 'pointer.png'
+}
+
+function getBodyArea() {
+  return bodyAreas[Math.abs(state.focus) % 3]
+}
+
+function getAreaItem(area) {
+  if (!area) {
+    let area = getBodyArea()
+  }
+  return Math.abs(state[area]) % 6
+}
+
 function upArrow() {
-  state.focus++
+  if (state.focus >= 1 && state.focus <= 2) {
+    state.focus--
+    moveArrow()
+  }
   printState()
 }
 
 function downArrow() {
-  state.focus--
+  if (state.focus >= 0 && state.focus <= 1) {
+    state.focus++
+    moveArrow()
+  }
   printState()
 }
 
 function leftArrow() {
-  let bodySection = dressing[Math.abs(state.focus) % 3]
-  state[bodySection]--
+  state[getBodyArea()]--
   printState()
+  dressClown()
 }
 
 function rightArrow() {
-  let bodySection = dressing[Math.abs(state.focus) % 3]
-  state[bodySection]++
+  state[getBodyArea()]++
   printState()
+  dressClown()
+}
+
+function printState() {
+  console.log(`focus is ${getBodyArea()}[${getAreaItem(getBodyArea())}]`)
+  console.log(state)
+  //   console.log(`head  ${state.head}  \t abs(${Math.abs(state.head) % 6})
+  // body  ${state.body}  \t abs(${Math.abs(state.body) % 6})
+  // shoes ${state.shoes} \t abs(${Math.abs(state.shoes) % 6})
+  // ----`)
 }
 
 function randomNumber() {
   return Math.floor(Math.random() * 6)
-}
-
-function printState() {
-  let bodySection = dressing[Math.abs(state.focus) % 3]
-  console.log(
-    ` dressing ${bodySection} item ${Math.abs(state[bodySection]) % 6}
-      focus ${state.focus} \t abs(${Math.abs(state.focus) % 3}) 
-      head  ${state.head}  \t abs(${Math.abs(state.head) % 6}) 
-      body  ${state.body}  \t abs(${Math.abs(state.body) % 6}) 
-      shoes ${state.shoes} \t abs(${Math.abs(state.shoes) % 6})`
-  )
 }
